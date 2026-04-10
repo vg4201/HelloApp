92f48dc09c726e0d63da8f598a96aebe21117bf3
vyshnavi <vg4201@srmist.edu.in>
2026-04-10 13:38:56 +0530

    Add UC4 feature

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,5 +1,19 @@
+import java.awt.*;
 public class HelloApp {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        StringBuilder namesBuilder = new StringBuilder();
+
+        if (args.length > 0) {
+            for (int i = 0; i < args.length; i++) {
+                namesBuilder.append(args[i]);
+                if (i < args.length - 1) {
+                    namesBuilder.append(", ");
+                }
+            }
+        } else {
+            namesBuilder.append("World");
+        }
+
+        System.out.println("Hello, " + namesBuilder.toString() + "!");
     }
 }
