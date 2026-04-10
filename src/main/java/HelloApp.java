67d7c83a75d87ffbab9170ba3df8c78366eafd30
vyshnavi <vg4201@srmist.edu.in>
2026-04-10 13:34:14 +0530

    Add HelloApp

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,5 +1,9 @@
 public class HelloApp {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+       String name = "World";
+       if(args.length > 0){
+           name = args[0];
+       }
+       System.out.println("Hello, " + name + "!");
     }
 }
