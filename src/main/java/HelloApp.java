a24e89ce9838477d34cc0b435d9824919d55960a
vyshnavi <vg4201@srmist.edu.in>
2026-04-10 14:01:49 +0530

    Finalize HelloApp after merges

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,6 +1,23 @@
-import java.awt.*;
 public class HelloApp {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+          
+            StringBuilder nameBuilder = new StringBuilder();
+
+           
+            for (String name : args) {
+                nameBuilder.append(name).append(", ");
+            }
+
+            String finalNames = "";
+            if (nameBuilder.length() > 0) {
+
+                finalNames = nameBuilder.substring(0, nameBuilder.length() - 2);
+            }
+            System.out.println("Hello, " + finalNames + "!");
+        }
     }
 }
