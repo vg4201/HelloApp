dcb60edd323548788cc133610154bc2c96290f30
vyshnavi <vg4201@srmist.edu.in>
2026-04-10 13:49:37 +0530

    Add UC6 feature

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,20 +1,18 @@
-import java.awt.*;
 public class HelloApp {
     public static void main(String[] args) {
-        StringBuilder namesBuilder = new StringBuilder();
-
         if (args.length == 0) {
-            namesBuilder.append("World");
+            System.out.println("Hello, World!");
         } else {
+            StringBuilder nameBuilder = new StringBuilder();
             for (String name : args) {
-
-                if (namesBuilder.length() > 0) {
-                    namesBuilder.append(", ");
-                }
-                namesBuilder.append(name);
+                nameBuilder.append(name).append(", ");
             }
-        }
+            String finalNames = "";
+            if (nameBuilder.length() > 0) {
 
-        System.out.println("Hello, " + namesBuilder.toString() + "!");
+                finalNames = nameBuilder.substring(0, nameBuilder.length() - 2);
+            }
+            System.out.println("Hello, " + finalNames + "!");
+        }
     }
 }
