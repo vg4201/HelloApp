5499924c64f5a480db2b189a8346abc422f6d744
vyshnavi <vg4201@srmist.edu.in>
2026-04-13 14:48:48 +0530

    feat: add UC7 Hello command-line args support

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,23 +1,14 @@
+
+
 public class HelloApp {
     public static void main(String[] args) {
-        
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
-          
-            StringBuilder nameBuilder = new StringBuilder();
+            String names = String.join(", ", args);
 
-           
-            for (String name : args) {
-                nameBuilder.append(name).append(", ");
-            }
-
-            String finalNames = "";
-            if (nameBuilder.length() > 0) {
-
-                finalNames = nameBuilder.substring(0, nameBuilder.length() - 2);
-            }
-            System.out.println("Hello, " + finalNames + "!");
+        
+            System.out.println("Hello, " + names + "!");
         }
     }
 }
